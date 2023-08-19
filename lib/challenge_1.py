8027d849b7260117a1f8e0557afa17a43ab6007b
Dennis Kimaita <dkimaita22@gmail.com>
2023-08-19 04:40:26 +0300

    change test data for more variety

--- lib/challenge_1.py
+++ lib/challenge_1.py
@@ -13,7 +13,7 @@ def convert_to_24hr_sys(hour,minutes, period):
 
 convert_to_24hr_sys(12, 00, 'pm')
 convert_to_24hr_sys(12, 00, 'am')
-convert_to_24hr_sys(1, 30, 'pm')
+convert_to_24hr_sys(1, 30, 'am')
 convert_to_24hr_sys(1, 30, 'pm')
 convert_to_24hr_sys(4, 30, 'am')
 convert_to_24hr_sys(9, 30, 'pm')
